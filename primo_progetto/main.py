823c9cf259b5fc39b0fc8dd52f21fd7af3487c1e
Elsacred02 <m.ferioli@campus.unimib.it>
2025-07-25 14:32:11 +0200

    Implementato metodo di jacobi correttamente

--- primo_progetto/main.py
+++ primo_progetto/main.py
@@ -1,7 +1,23 @@
 from solutori_lineari_iterativi.solvers import *
+import numpy as np
 
 def main():
-    base_iterative_solver()
+    # Matrice A (esempio semplice: diagonale dominante)
+    A = np.array([[4.0, 1.0],
+                  [2.0, 3.0]])
+
+    # Vettore termine noto b
+    b = np.array([1.0, 2.0])
+
+    # Vettore iniziale x0 (stima iniziale)
+    x = np.linalg.solve(A, b)
+
+    # Tolleranza per il criterio di arresto
+    tol = 1e-6
+
+    sol, k, relative_error, elapsed_time = jacobi_solver(A, b, x, tol)
+
+    print(sol, k, relative_error, elapsed_time)
 
 if __name__ == '__main__':
     main()
--- solutori_lineari_iterativi/solvers.py
+++ solutori_lineari_iterativi/solvers.py
@@ -1,10 +1,7 @@
 import numpy as np
 import time
 
-from PyQt5.QtQml import kwargs
-
-
-def base_iterative_solver(A, b, x, tol, update_method):
+def _base_iterative_solver(A, b, x, tol, update_method):
 
     # Controlli preliminari sull'input
     rows, cols = A.shape
@@ -18,23 +15,53 @@ def base_iterative_solver(A, b, x, tol, update_method):
         print("[Errore] : il vettore x inserito non ha la stessa dimensione delle righe della matrice A")
         return None, 0, 0, 0
 
+    #Avvio cronometro
+    start_time = time.time()
+
     # Inizializzo la soluzione iniziale ed il contatore delle iterazioni
-    sol = np.zeros(A.shape[0])
+    sol = np.zeros(rows)
     k = 0
 
-    while (np.linalg.norm(np.dot(A, sol) - b) / np.linalg.norm(b)) > tol:
-        sol = update_method(A, b, sol)
+    while (np.linalg.norm(A @ sol - b, ord=np.inf) / np.linalg.norm(b)) > tol:
+        sol = update_method(sol)
+        k = k + 1
+        if k > 20000:
+            print("[Errore] : il metodo non converge")
+            return None, 0, 0, 0
+
+    # Fermo cronometro
+    elapsed_time = time.time() - start_time
+
+    relative_error = np.linalg.norm(A @ sol - b, ord=np.inf) / np.linalg.norm(b, ord=np.inf)
+
+    return sol, k, relative_error, elapsed_time
+
+
+def jacobi_solver(A, b, x, tol):
+
+    P = np.diag(np.diag(A))
+    P_inv = np.linalg.inv(P)
+    def _jacobi_update(current_sol):
+        new_sol = current_sol + P_inv @ (b - A @ current_sol)
+        return new_sol
+
+    return _base_iterative_solver(A, b, x, tol, _jacobi_update)
 
 
 
-def _jacobi_update(A, b, current_sol):
-    print("Ciao sono jacobi")
-
-def _gauss_seidel_update(A, b, current_sol):
+def gauss_seidel_update(A, current_sol, b):
     print("Ciao sono gauss seidel")
+    print(A)
+    print(current_sol)
+    print(b)
 
-def _gradient_update(A, current_sol, **kwargs):
+def gradient_update(A, current_sol, b):
     print("Ciao sono gradient")
+    print(A)
+    print(current_sol)
 
-def _coniugate_gradient_update(A, current_sol, b):
+def coniugate_gradient_update(A, current_sol, b):
     print("Ciao sono coniugate gradient")
+    print(A)
+    print(current_sol)
+    print(b)
