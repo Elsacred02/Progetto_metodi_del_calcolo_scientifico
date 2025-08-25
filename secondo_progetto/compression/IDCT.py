fb38b2446942e026aaa033b0f323d1c64d62df50
Elsacred02 <m.ferioli@campus.unimib.it>
2025-08-25 10:22:33 +0200

    refactoring

--- secondo_progetto/compression/IDCT.py
+++ secondo_progetto/compression/IDCT.py
@@ -1,26 +1,26 @@
 import numpy as np
-def IDCT1(coeffs):
-    N = len(coeffs)
-    x = np.zeros(N)
+def IDCT1(c):
+    N = len(c)
+    f = np.zeros(N)
 
-    for n in range(N):
+    for j in range(N):
 
         # Qui esce cos(0) quindi semplifico la scrittura dell'operazione
-        s = coeffs[0] * np.sqrt(1/N)
+        s = c[0] * np.sqrt(1 / N)
 
         for k in range(1, N):
-            s += coeffs[k] * np.sqrt(2/N) * np.cos(np.pi * k * (2*n+1) / (2*N))
-        x[n] = s
+            s += c[k] * np.sqrt(2 / N) * np.cos(np.pi * k * (2 * j + 1) / (2 * N))
+        f[j] = s
 
-    return x
+    return f
 
-def IDCT2(coeffs):
-    row, columns = coeffs.shape
+def IDCT2(A):
+    row, columns = A.shape
     original_matrix = np.zeros((row, columns))
 
     # inversa sulle colonne
     for k in range(columns):
-        original_matrix[:, k] = IDCT1(coeffs[:, k])
+        original_matrix[:, k] = IDCT1(A[:, k])
 
     # inversa sulle righe
     for k in range(row):
